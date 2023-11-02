a8cbd3de7cb170d13317de2fea1bd2444ad42b76
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-11-02 19:43:36 +0300

    Unit test fix

--- client_test.py
+++ client_test.py
@@ -28,17 +28,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
 
+    prices = {}
     for quote in quotes:
-      self.assertEqual(getRatio(quote['top_bid']['price'], 0), None)
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices[stock] = price
+    self.assertEqual(getRatio(prices['ABC'], 0), None)
 
   def test_getRatio_priceAIsZero(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-
+    prices = {}
     for quote in quotes:
-      self.assertEqual(getRatio(0, quote['top_bid']['price']), 0)
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices[stock] = price
+    self.assertEqual(getRatio(0, prices['DEF']), 0)
 
 if __name__ == '__main__':
     unittest.main()
